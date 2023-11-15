35a9629fd3b3086952d1c752014a90b46d15bf64
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-11-15 13:32:15 +0800

    added images and footer styles

--- frontend/src/component/navbar_and_footer/footer.js
+++ frontend/src/component/navbar_and_footer/footer.js
@@ -3,102 +3,99 @@ import "./footer.css"
 const Footer = () => {
     return(
         <>
-            <footer class="footer container-fluid px-4 py-4 bg-dark">
-                <div class="row flex-wrap align-items-top justify-content-space-center">
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 text-center text-white px-2 footer-text-and-logo">
+            <footer className="footer container-fluid px-4 pt-5 pb-2 bg-dark">
+                <div className="row flex-wrap align-items-top justify-content-space-between pb-5">
+                    <div className="footer_logo col-lg-2 col-md-4 col-sm-4 col-12 text-start text-white px-2 py-2">
                         <a href="index.html">
-                            {/* <img src="./logo-img.png" class="logo-img mt-4" alt=""> */}
+                            <img src="../images/footer_logo.svg" className="logo-img mt-4" alt=""/>
                         </a>
-                        <p class="mt-2">Your Source for Electrical Excellence</p>
                     </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 customer-service text-white text-start px-2">
-                        <h5 class="fw-semibold mb-3">CUSTOMER SERVICE</h5>
+                    <div className="customer_service col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start px-2 py-2">
+                        <h5 className="fw-semibold mb-3">CUSTOMER SERVICE</h5>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Contact Us</a>
+                            <a href="" className="text-decoration-none text-white">Contact Us</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Shipping Information</a>
+                            <a href="" className="text-decoration-none text-white">Shipping Information</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Return & Refund</a></p>
+                            <a href="" className="text-decoration-none text-white">Return & Refund</a></p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">FAQs</a>
+                            <a href="" className="text-decoration-none text-white">FAQs</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Privacy Policy</a>
+                            <a href="" className="text-decoration-none text-white">Privacy Policy</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Terms and Condition</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 quick-links text-white text-start px-2">
-                        <h5 class="fw-semibold mb-3">QUICK LINKS</h5>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Home</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Products</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Account</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Blog</a>
+                            <a href="" className="text-decoration-none text-white">Terms and Condition</a>
                         </p>
                     </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 quick-links text-white text-start px-2">
-                        <h5 class="fw-semibold mb-3">MY ACCOUNT</h5>
+                    <div className="categories col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start px-2 py-2">
+                        <h5 className="fw-semibold mb-3">CATEGORIES</h5>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Login</a>
+                            <a href="" className="text-decoration-none text-white">Tools</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Logout</a>
+                            <a href="" className="text-decoration-none text-white">Protection</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">My Cart</a>
+                            <a href="" className="text-decoration-none text-white">Lighting</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">My Wishlist</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 quick-links text-white text-start px-2">
-                        <h5 class="fw-semibold mb-3">CATEGORIES</h5>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Tools</a>
+                            <a href="" className="text-decoration-none text-white">Wires</a>
                         </p>
                         <p>
-                            <a href="" class="text-decoration-none text-white">Protection</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Lighting</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Wires</a>
-                        </p>
-                        <p>
-                            <a href="" class="text-decoration-none text-white">Outlets</a>
+                            <a href="" className="text-decoration-none text-white">Outlets</a>
                         </p>
                     </div>
-                    <div class="col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start social-links px-2">
-                        <h5 class="fw-semibold mb-3">SOCIAL LINKS</h5>
-                        <div class="social-icons d-flex align-items-center justify-content-start">
-                            <a href="" class="mx-2 text-white">
-                                <i class="bi bi-facebook fs-4"></i>
+                    <div className="quick_links col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start px-2 py-2">
+                        <h5 className="fw-semibold mb-3">QUICK LINKS</h5>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Home</a>
+                        </p>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Products</a>
+                        </p>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Account</a>
+                        </p>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Blog</a>
+                        </p>
+                    </div>
+                    <div className="my_account col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start px-2 py-2">
+                        <h5 className="fw-semibold mb-3">MY ACCOUNT</h5>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Cart</a>
+                        </p>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">Checkout</a>
+                        </p>
+                        <p>
+                            <a href="" className="text-decoration-none text-white">My Wishlist</a>
+                        </p>
+                    </div>
+                    
+                    <div className="social_links col-lg-2 col-md-4 col-sm-4 col-12 text-white text-start px-2 py-2">
+                        <h5 className="fw-semibold mb-3">SOCIAL LINKS</h5>
+                        <div className="social_icons align-items-center justify-content-start">
+                            <a href="" className="mx-2 text-white">
+                                <i className="bi bi-facebook fs-4"></i>
                             </a>
-                            <a href="" class="mx-2 text-white">
-                                <i class="bi bi-instagram fs-4"></i>
+                            <a href="" className="mx-2 text-white">
+                                <i className="bi bi-instagram fs-4"></i>
                             </a>
-                            <a href="" class="mx-2 text-white">
-                                <i class="bi bi-tiktok fs-4"></i>
+                            <a href="" className="mx-2 text-white">
+                                <i className="bi bi-tiktok fs-4"></i>
                             </a>
-                            <a href="" class="mx-2 text-white">
-                                <i class="bi bi-envelope fs-4"></i>
+                            <a href="" className="mx-2 text-white">
+                                <i className="bi bi-envelope fs-4"></i>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="footer_copyright container-fluid px-4 pt-4 pb-0">
-                    <p class="text-white my-auto text-center">Copyright © 2023 SparkSource Central. All rights reserved.</p>
+                <div className="footer_copyright container-fluid px-4 pt-4 mt-4 pb-0">
+                    <p className="text-white my-auto text-center">Copyright © 2023 SparkSource Central. All rights reserved.</p>
                 </div>
             </footer>
         </>
