169b8a77f36b717652efcd1284074005b6c0f5d1
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-11-16 19:24:35 +0800

    renamed the components and added css properties and styles

---
+++ frontend/src/component/navbar_and_footer/navbar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import './navbar.css';
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    return(
+        <>
+            <header className="position-fixed container-fluid m-0 p-0 mb-5">
+                <div className="container-fluid py-2 px-4">
+                    <div className="row align-items-center justify-content-center text-center">
+                        <div className="col-md-2 text-center text-md-start">
+                            <img src="../images/spark_source_central_logo.svg" alt="Logo" className="img-fluid"/>
+                        </div>
+                        <div className="col-md-8 text-center mb-2 mt-2">
+                            <form className="d-flex">
+                                <input className="form-control search_input" type="search" placeholder="Search for products..." aria-label="Search"/>
+                                <button className="btn search_btn" type="submit">
+                                    <i className="bi bi-search text-white"></i>
+                                </button>
+                            </form>
+                        </div>
+                        <div className="col-md-2 text-center">
+                            <div className="account_info d-flex justify-content-end align-items-center text-white mt-3">
+                                <a className="text-white position-relative nav-link me-3">
+                                    <i className="bi bi-bag fs-4"></i>
+                                    <span className="position-absolute top-0 start-1 translate-middle badge rounded-pill bg-danger">
+                                        0
+                                    </span>
+                                </a>
+                                <a className="text-white position-relative nav-link me-3">
+                                    <i className="bi bi-heart fs-4"></i>
+                                    <span className="position-absolute top-0 start-1 translate-middle badge rounded-pill bg-danger">
+                                        0
+                                    </span>
+                                </a>
+                                <a href="#" className="text-white">
+                                    <i className="bi bi-person fs-2"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <nav className="navbar navbar-expand-lg navbar-dark py-2">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav mx-auto">
+                                <li className="nav-item me-3">
+                                    <a className="nav-link text-white fw-semibold" href="#"><i className="bi bi-lightning"></i> ALL ITEMS</a>
+                                </li>
+                                <li className="nav-item me-3">
+                                    <a className="nav-link text-white fw-semibold" href="#"><i className="bi bi-lightbulb"></i> LIGHTING FIXTURES</a>
+                                </li>
+                                <li className="nav-item me-3">
+                                    <a className="nav-link text-white fw-semibold" href="#"><i className="bi bi-toggle-on"></i> SWITCHES & OUTLETS</a>
+                                </li>
+                                <li className="nav-item me-3">
+                                    <a className="nav-link text-white fw-semibold" href="#"><i className="bi bi-shield-lock"></i> SAFETY & PROTECTION</a>
+                                </li>
+                                <li className="nav-item me-3">
+                                    <a className="nav-link text-white fw-semibold" href="#"><i className="bi bi-tools"></i>  TOOLS & ACCESSORIES</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Navbar;
--- frontend/src/component/pages/home.jsx
+++ frontend/src/component/pages/home.jsx
@@ -5,33 +5,33 @@ const Home = () => {
     return(
         <>
             {/* Hero Section Start */}
-            <section class="hero_section container-fluid vh-100 d-flex align-items-center justify-content-center py-5 px-4">
-                <div class="hero_text text-center py-2 px-2 mt-5 text-white">
-                    <h1 class="fw-bolder mt-4 text-uppercase">Discover Excellence in Electrical Innovation</h1>
-                    <p class="fw-medium mt-4 fs-3">Find High-Quality Electrical Products at Affordable Prices.</p>
-                    <div class="text-center mb-2">
-                        <a href="#"  class="btn shopnow_btn my-2 mx-2 px-4 py-3 fw-semibold">SHOP NOW</a>
+            <section className="hero_section container-fluid vh-100 d-flex align-items-center justify-content-center py-5 px-4">
+                <div className="hero_text text-center py-2 px-2 mt-5 text-white">
+                    <h1 className="fw-bolder mt-4 text-uppercase">Discover Excellence in Electrical Innovation</h1>
+                    <p className="fw-medium mt-4 fs-3">Find High-Quality Electrical Products at Affordable Prices.</p>
+                    <div className="text-center mb-2">
+                        <a href="#"  className="btn shopnow_btn my-2 mx-2 px-4 py-3 fw-semibold">SHOP NOW</a>
                     </div>    
                 </div>
             </section>
             {/* Hero Section end */}
 
-            {/*  */}
+            {/* About Section Start */}
             <section className="about_section container_fluid px-4 pt-5 mt-5">
                 <div className="container">
-                    <div class="row">
+                    <div className="row">
                         <div className="features col-lg-3 col-md-4 col-sm-6 col-12 d-flex align-items-center justify-content-between mb-4">
                             <div className="icon me-4">
                                 <i class="fa-solid fa-truck"></i>
                             </div>
                             <div className="detail">
                                 <h3 className="fw-bolder">FREE SHIPPING</h3>
-                                <p>Order above $1000</p>
+                                <p>Order above ₱10,000</p>
                             </div>
                         </div>
                         <div className="features col-lg-3 col-md-4 col-sm-6 col-12x d-flex align-items-center justify-content-between mb-4">
                             <div className="icon me-4">
-                                <i class="fa-solid fa-peso-sign"></i>
+                                <i class="fa-solid fa-right-left"></i>
                             </div>
                             <div className="detail">
                                 <h3 className="fw-bolder">RETURN AND REFUND</h3>
@@ -40,14 +40,14 @@ const Home = () => {
                         </div>
                         <div className="features col-lg-3 col-md-4 col-sm-6 col-12 d-flex align-items-center justify-content-between mb-4">
                             <div className="icon me-4">
-                                <i class="fa-solid fa-percent"></i>
+                                <i class="fa-solid fa-trophy"></i>
                             </div>
                             <div className="detail">
-                                <h3 className="fw-bolder">MEMBER DISCOUNT</h3>
-                                <p>On Every Order</p>
+                                <h3 className="fw-bolder">HIGH QUALITY</h3>
+                                <p>Durable Products</p>
                             </div>
                         </div>
-                        <div className="features col-lg-3 col-md-4 col-sm-6 col-12 d-flex align-items-center justify-content-between">
+                        <div className="features col-lg-3 col-md-4 col-sm-6 col-12 d-flex align-items-center justify-content-between mb-4">
                             <div className="icon me-4">
                                 <i class="fa-solid fa-headset"></i>
                             </div>
@@ -60,34 +60,34 @@ const Home = () => {
                     
                 </div>
             </section>
-            {/*  */}
+            {/* About Section End */}
 
             {/* <!-- Top Products Section --> */}
-            <section class="top_products_section container-fluid py-5 px-4">
-                <div class="container-fluid">
-                    <h2 class="text-center fw-bolder fs-2 mt-5 mb-5 pb-5">Leading the charge with innovative electrical solutions, empowering you to <span class="hashtag">#EnergizeResponsibly</span>.</h2>
-                    <h2 class="my-5 text-center fw-bolder">TOP PRODUCTS</h2>
-                    <div class="row mt-5 row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+            <section className="top_products_section container-fluid py-5 px-4">
+                <div className="container-fluid">
+                    <h2 className="text-center fw-bolder fs-2 mt-5 mb-5 pb-5">Leading the charge with innovative electrical solutions, empowering you to <span className="hashtag">#EnergizeResponsibly</span>.</h2>
+                    <h2 className="my-5 text-center fw-bolder">TOP PRODUCTS</h2>
+                    <div className="row mt-5 row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                         {
                             Topproducts.map((currentElement) => 
                             {
                                 return(
-                                    <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4" key={currentElement.id}>
-                                        <div class="card text-center">
-                                            <img src={currentElement.Image} class="card-img-top img-fluid" alt={currentElement.Title}/>
-                                            <div class="card-body mt-0 pt-1">
-                                                <p class="card-text text-secondary text-uppercase fs-6">{currentElement.Category}</p>
-                                                <h5 class="card-title fw-medium text-uppercase">{currentElement.Title}</h5>
-                                                <p class="card-price fw-bold fs-4">₱{currentElement.Price}</p>
-                                                <div class="d-flex justify-content-center align-items-center">
-                                                    <a href="#" class="btn me-2 add_to_cart">
-                                                        <i class="bi bi-bag-plus"></i>
+                                    <div className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4" key={currentElement.id}>
+                                        <div className="card text-center">
+                                            <img src={currentElement.Image} className="card-img-top img-fluid" alt={currentElement.Title}/>
+                                            <div className="card-body mt-0 pt-1">
+                                                <p className="card-text text-secondary text-uppercase fs-6">{currentElement.Category}</p>
+                                                <h5 className="card-title fw-medium text-uppercase">{currentElement.Title}</h5>
+                                                <p className="card-price fw-bold fs-4">₱{currentElement.Price}</p>
+                                                <div className="d-flex justify-content-center align-items-center">
+                                                    <a href="#" className="btn me-2 add_to_cart">
+                                                        <i className="bi bi-bag-plus"></i>
                                                     </a>
-                                                    <a href="#" class="btn me-2 add_to_wishlist">
-                                                        <i class="bi bi-heart"></i>
+                                                    <a href="#" className="btn me-2 add_to_wishlist">
+                                                        <i className="bi bi-heart"></i>
                                                     </a>
-                                                    <a href="#" class="btn view_detail">
-                                                        <i class="bi bi-eye"></i>
+                                                    <a href="#" className="btn view_detail">
+                                                        <i className="bi bi-eye"></i>
                                                     </a>                      
                                                 </div>                         
                                             </div>
@@ -102,32 +102,32 @@ const Home = () => {
             </section>
 
             {/* <!-- Product Categories Section --> */}
-            <section class="product-categories-section">
-                <div class="container text-center">
+            <section className="product-categories-section">
+                <div className="container text-center">
                 <h2>Product Categories</h2>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="category">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="category">
                             <img src="category1-icon.png" alt="Category 1"/>
                             <h4>Lighting</h4>
-                            <a href="#" class="btn btn-outline-primary">Shop Now</a>
+                            <a href="#" className="btn btn-outline-primary">Shop Now</a>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div className="col-md-4">
                     {/* <!-- Repeat similar structure for other categories -->
                     <!-- Example: Wiring category --> */}
-                    <div class="category">
+                    <div className="category">
                             <img src="category1-icon.png" alt="Category 1"/>
                             <h4>Lighting</h4>
-                            <a href="#" class="btn btn-outline-primary">Shop Now</a>
+                            <a href="#" className="btn btn-outline-primary">Shop Now</a>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div className="col-md-4">
                     {/* <!-- Example: Tools category --> */}
-                        <div class="category">
+                        <div className="category">
                             <img src="category1-icon.png" alt="Category 1"/>
                             <h4>Lighting</h4>
-                            <a href="#" class="btn btn-outline-primary">Shop Now</a>
+                            <a href="#" className="btn btn-outline-primary">Shop Now</a>
                         </div>
                     </div>
                 </div>
@@ -135,18 +135,18 @@ const Home = () => {
             </section>
 
             {/* <!-- Special Offers Section --> */}
-            <section class="special-offers-section">
-                <div class="container text-center">
+            <section className="special-offers-section">
+                <div className="container text-center">
                 <h2>Special Offers</h2>
-                <div class="row">
+                <div className="row">
                     {/* <!-- Display special offers here -->
                     <!-- Sample offer card --> */}
-                    <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                        <h5 class="card-title">Limited Time Offer</h5>
-                        <p class="card-text">Get 20% off on selected products.</p>
-                        <a href="#" class="btn btn-primary">Shop Now</a>
+                    <div className="col-md-4">
+                    <div className="card">
+                        <div className="card-body">
+                        <h5 className="card-title">Limited Time Offer</h5>
+                        <p className="card-text">Get 20% off on selected products.</p>
+                        <a href="#" className="btn btn-primary">Shop Now</a>
                         </div>
                     </div>
                     </div>
@@ -156,14 +156,14 @@ const Home = () => {
             </section>
 
             {/* <!-- Customer Testimonials Section --> */}
-            <section class="customer-testimonials-section">
-                <div class="container text-center">
+            <section className="customer-testimonials-section">
+                <div className="container text-center">
                 <h2>Customer Testimonials</h2>
-                <div class="row">
+                <div className="row">
                     {/* <!-- Display customer testimonials here -->
                     <!-- Sample testimonial --> */}
-                    <div class="col-md-6">
-                    <div class="testimonial">
+                    <div className="col-md-6">
+                    <div className="testimonial">
                         <p>"Great quality products and excellent service!"</p>
                         <p>- John Doe</p>
                     </div>
@@ -174,8 +174,8 @@ const Home = () => {
             </section>
 
             {/* <!-- Value Proposition Section --> */}
-            <section class="value-proposition-section">
-                <div class="container text-center">
+            <section className="value-proposition-section">
+                <div className="container text-center">
                 <h2>Why Choose Us?</h2>
                 <p>We offer top-quality electrical supplies with...</p>
                 <ul>
@@ -187,20 +187,20 @@ const Home = () => {
             </section>
 
             {/* <!-- Call-to-Action Section --> */}
-            <section class="cta-section text-center">
-                <div class="container">
+            <section className="cta-section text-center">
+                <div className="container">
                 <h2>Subscribe for Updates</h2>
                 <p>Sign up for our newsletter to receive exclusive offers and updates.</p>
-                <form action="#" method="post" class="subscribe-form">
+                <form action="#" method="post" className="subscribe-form">
                     <input type="email" name="email" placeholder="Enter your email" required/>
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
+                    <button type="submit" className="btn btn-primary">Subscribe</button>
                 </form>
                 </div>
             </section>
 
             {/* <!-- Trust Badges or Certifications Section --> */}
-            <section class="trust-badges-section text-center">
-                <div class="container">
+            <section className="trust-badges-section text-center">
+                <div className="container">
                 <h2>Our Certifications</h2>
                 {/* <!-- Display trust badges or certifications here --> */}
                 <img src="certification1.png" alt="Certification 1"/>
