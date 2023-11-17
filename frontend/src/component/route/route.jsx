27f0f34c2e21f46a88fcbf521f4d897e9763ac86
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-11-17 20:02:14 +0800

    added login and register components

--- frontend/src/component/route/route.jsx
+++ frontend/src/component/route/route.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Product from "../pages/product";
+
 const Routeway = () => {
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/product" element={<Product/>}/>
+            <Route/>
         </Routes>
     )
 }
