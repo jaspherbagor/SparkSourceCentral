a727872a1c6e09d33b643fd244d1728faa1fb862
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-11-17 16:06:55 +0800

    added contents to the all products and add elements the product page

--- frontend/src/component/pages/product.jsx
+++ frontend/src/component/pages/product.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../styles/product.css";
+import AllProducts from "../resources/Allproducts";
 const Product = () => {
     return(
         <> 
-            <div class="product_page container-fluid bg-secondary px-4 py-2">
+            <div class="product_page container-fluid bg-secondary px-4 py-5">
                 <div class="row">
                     {/* <!-- Product categories column --> */}
                     <div class="col-lg-3 col-md-3 col-sm-3 product_category">
@@ -18,8 +19,37 @@ const Product = () => {
                     {/* <!-- Product cards column --> */}
                     <div class="col-lg-9 col-md-9 col-sm-9">
                         <div class="row">
+                            {
+                                AllProducts.map((currentElement) => 
+                                {
+                                    return(
+                                        <div className="col-lg-4 col-md-4 col-sm-6 mb-3" key={currentElement.id}>
+                                            <div className="product_card card text-center">
+                                                <img src={currentElement.Image} className="card-img-top img-fluid" alt={currentElement.Title}/>
+                                                <div className="card-body mt-0 pt-1">
+                                                    <p className="card-text text-secondary text-uppercase fs-6">{currentElement.Category}</p>
+                                                    <h5 className="card-title fw-medium text-uppercase">{currentElement.Title}</h5>
+                                                    <p className="card-price fw-bold fs-4">₱{currentElement.Price}</p>
+                                                    <div className="d-flex justify-content-center align-items-center">
+                                                        <a href="#" className="btn me-2 add_to_cart">
+                                                            <i className="bi bi-bag-plus"></i>
+                                                        </a>
+                                                        <a href="#" className="btn me-2 add_to_wishlist">
+                                                            <i className="bi bi-heart"></i>
+                                                        </a>
+                                                        <a href="#" className="btn view_detail">
+                                                            <i className="bi bi-eye"></i>
+                                                        </a>                      
+                                                    </div>                         
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
                         {/* <!-- Product card 1 --> */}
-                            <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+                            {/* <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
                                 <div class="card">
                                     <img src="product1.jpg" class="card-img-top" alt="Product 1"/>
                                     <div class="card-body">
@@ -28,9 +58,9 @@ const Product = () => {
                                         <a href="#" class="btn btn-primary">Buy Now</a>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
                             {/* <!-- Product card 2 --> */}
-                            <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+                            {/* <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
                                 <div class="card">
                                     <img src="product2.jpg" class="card-img-top" alt="Product 2"/>
                                     <div class="card-body">
@@ -39,9 +69,9 @@ const Product = () => {
                                         <a href="#" class="btn btn-primary">Buy Now</a>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
                             {/* <!-- Product card 3 --> */}
-                            <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+                            {/* <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
                                 <div class="card">
                                     <img src="product3.jpg" class="card-img-top" alt="Product 3"/>
                                     <div class="card-body">
@@ -50,7 +80,7 @@ const Product = () => {
                                         <a href="#" class="btn btn-primary">Buy Now</a>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
---
+++ frontend/src/component/resources/Allproducts.jsx
@@ -0,0 +1,61 @@
+const AllProducts =
+[
+    {
+        id: 1,
+        Title: "Safety Shoes",
+        Category: "Safety & Protection",
+        Price: "1600",
+        Image: "../images/Safety_Shoes.jpg"
+    },
+    {
+        id: 2,
+        Title: "Cordless Drill",
+        Category: "Tools & Accessories",
+        Price: "1800",
+        Image: "../images/Cordless_Drill.jpg"
+    },
+    {
+        id: 3,
+        Title: "GFCI Outlet",
+        Category: "Switches & Outlets",
+        Price: "1500",
+        Image: "../images/GFCI_Outlet.jpg"
+    },
+    {
+        id: 4,
+        Title: "Safety Gloves",
+        Category: "Safety & Protection",
+        Price: "450",
+        Image: "../images/Insulated_Gloves.jpg"
+    },
+    {
+        id: 5,
+        Title: "Timer Switch",
+        Category: "Switches & Outlets",
+        Price: "1500",
+        Image: "../images/Timer_Switch.png"
+    },
+    {
+        id: 6,
+        Title: "Circuit Finder",
+        Category: "Tools & Accessories",
+        Price: "1290",
+        Image: "../images/Circuit_Finder.jpg"
+    },
+    {
+        id: 7,
+        Title: "Wall Sconce",
+        Category: "Lighting Fixtures",
+        Price: "4600",
+        Image: "../images/Wall_Sconce.webp"
+    },
+    {
+        id: 7,
+        Title: "Light Chandelier",
+        Category: "Lighting Fixtures",
+        Price: "12900",
+        Image: "../images/Light Chandelier.webp"
+    },
+]
+
+export default AllProducts;
