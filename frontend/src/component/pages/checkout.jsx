93f6774ad57ed10e1a5e9fa2fbaa3a55470997e3
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-11-30 14:41:46 +0800

    added cash-on-delivery option

--- frontend/src/component/pages/checkout.jsx
+++ frontend/src/component/pages/checkout.jsx
@@ -26,9 +26,7 @@ const Checkout = () => {
         ContactNumber: '',
         City: '',
         Province: '',
-        ZipCode: '',
-        CardName: '',
-        CardNumber: ''
+        ZipCode: ''
     });
 
     const [submitted, setSubmitted] = useState(false);
@@ -57,8 +55,6 @@ const Checkout = () => {
             City: users.City,
             Province: users.Province,
             ZipCode: users.ZipCode,
-            CardName: users.CardName,
-            CardNumber: users.CardNumber,
             orderDetails: orderDetails, // Include cart items, quantities, and subtotals
             orderTotal: orderTotal // Include the total amount
         };
@@ -71,9 +67,7 @@ const Checkout = () => {
             !users.ContactNumber ||
             !users.City ||
             !users.Province ||
-            !users.ZipCode ||
-            !users.CardName ||
-            !users.CardNumber
+            !users.ZipCode
         ) {
             // alert("Please fill in all required fields");
             const toastLive = document.getElementById('liveToast');
@@ -124,9 +118,7 @@ const Checkout = () => {
                     ContactNumber: '',
                     City: '',
                     Province: '',
-                    ZipCode: '',
-                    CardName: '',
-                    CardNumber: ''
+                    ZipCode: ''
                 });
                 setSubmitted(true); // Update the submission status
                 setTimeout(function() {
@@ -170,64 +162,42 @@ const Checkout = () => {
                             <form>
 
                                 <div className="row">
-                                    <div className="col-md-6">
-                                        <h3 className="fw-bold">Billing Address</h3>
+                                    <div className="col-md-12">
+                                        <h3 className="fw-bold">Shipping Address</h3>
                                         <label htmlFor="FullName" className="text-black"><i className="bi bi-person"></i> Full Name</label>
-                                        <input type="text" className="form-control mb-3" value={users.FullName} onChange={data} id="FullName" name="FullName" placeholder="Juan M. Dela Cruz"/>
+                                        <input type="text" className="form-control mb-3" value={users.FullName} onChange={data} id="FullName" name="FullName"/>
                                         <label htmlFor="Email" className="text-black"><i className="bi bi-envelope"></i> Email</label>
-                                        <input type="text" className="form-control mb-3" value={users.Email} onChange={data} id="Email" name="Email" placeholder="juan@example.com"/>
+                                        <input type="text" className="form-control mb-3" value={users.Email} onChange={data} id="Email" name="Email"/>
                                         <label htmlFor="Address" className="text-black"><i className="bi bi-geo-alt"></i> Address</label>
-                                        <input type="text" className="form-control mb-3" value={users.Address} onChange={data} id="Address" name="Address" placeholder="5A 395 Banawe Street"/>
+                                        <input type="text" className="form-control mb-3" value={users.Address} onChange={data} id="Address" name="Address"/>
                                         <label htmlFor="ContactNumber" className="text-black"><i className="bi bi-telephone"></i> Contact Number</label>
-                                        <input type="text" className="form-control mb-3" value={users.ContactNumber} onChange={data}  id="ContactNumber" name="ContactNumber" placeholder="09092347659"/>
+                                        <input type="text" className="form-control mb-3" value={users.ContactNumber} onChange={data}  id="ContactNumber" name="ContactNumber"/>
                                         <label htmlFor="City" className="text-black"><i className="bi bi-building"></i> City</label>
-                                        <input type="text" className="form-control mb-3" value={users.City} onChange={data} id="City" name="City" placeholder="Labason"/>
+                                        <input type="text" className="form-control mb-3" value={users.City} onChange={data} id="City" name="City"/>
 
                                         <div className="row">
                                         <div className="col-md-6">
                                             <label htmlFor="Province" className="text-black">Province</label>
-                                            <input type="text" className="form-control mb-3" value={users.Province} onChange={data} id="Province" name="Province" placeholder="Zamboanga Del Norte"/>
+                                            <input type="text" className="form-control mb-3" value={users.Province} onChange={data} id="Province" name="Province"/>
                                         </div>
                                         <div className="col-md-6">
                                             <label htmlFor="ZipCode" className="text-black">Zip/Postal Code</label>
-                                            <input type="text" className="form-control mb-3" value={users.ZipCode} onChange={data} id="ZipCode" name="ZipCode" placeholder="7100"/>
+                                            <input type="text" className="form-control mb-3" value={users.ZipCode} onChange={data} id="ZipCode" name="ZipCode"/>
                                         </div>
                                         </div>
                                     </div>
-
-                                    <div className="col-md-6">
-                                        <h3 className="fw-bold">Payment Details</h3>
-                                        <label htmlFor="fname" className="text-black">Accepted Card</label>
-                                        <div className="icon-container mb-3">
-                                            <i className="fa-brands fa-cc-visa text-primary fs-2 me-2" ></i>
-                                            <i className="fa-brands fa-cc-mastercard text-danger fs-2 me-2"></i>
-                                            <i className="fa-brands fa-cc-jcb text-success fs-2 me-2"></i>
-                                            <i className="fa-brands fa-cc-amex text-secondary fs-2 me-2"></i>
-                                        </div>
-                                        <label htmlFor="CardName" className="text-black">Name on Card</label>
-                                        <input type="text" className="form-control mb-3" value={users.CardName} onChange={data} id="CardName" name="CardName" placeholder="John More Doe"/>
-                                        <label htmlFor="CardNumber" className="text-black">Credit card number</label>
-                                        <input type="text" className="form-control mb-3" value={users.CardNumber} onChange={data} id="CardNumber" name="CardNumber" placeholder="1111-2222-3333-4444"/>
-                                        <label htmlFor="expmonth" className="text-black">Exp Month</label>
-                                        <input type="text" className="form-control mb-3" id="expmonth" name="expmonth" placeholder="September"/>
-
-                                        <div className="row">
-                                        <div className="col-md-6">
-                                            <label htmlFor="expyear" className="text-black">Exp Year</label>
-                                            <input type="text" className="form-control mb-3" id="expyear" name="expyear" placeholder="2018"/>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <label htmlFor="cvv" className="text-black">CVV</label>
-                                            <input type="text" className="form-control mb-3" id="cvv" name="cvv" placeholder="352"/>
-                                        </div>
+                                </div>
+                                <div className="mb-5">
+                                    <h2 className="fw-bolder mt-4">Mode of Payment</h2>
+                                    <div className="form-check">
+                                        <input className="form-check-input" defaultChecked type="radio" name="cashondelivery" id="cashondelivery"/>
+                                        <label className="form-check-label text-black" for="cashondelivery">
+                                            <img src="../images/CASH-ON-DELIVERY.png" />
+                                        </label>
                                     </div>
                                 </div>
+                                
 
-                                </div>
-                                <div className="form-check">
-                                    <input type="checkbox" className="form-check-input" defaultChecked name="sameadr" id="sameadr"/>
-                                    <label className="form-check-label text-black" htmlFor="sameadr"> Shipping address same as billing</label>
-                                </div>
                                 <div className="d-grid text-start mt-4">
                                     <button type="submit" className="btn place_order_btn fw-semibold px-3 py-2" onClick={sendData}>
                                         Place Order <i className="bi bi-lock-fill"></i> @ ₱<span>{totalPrice.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
